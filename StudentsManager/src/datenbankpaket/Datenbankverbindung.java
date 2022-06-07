a0b73440d80c439fd8e231a0a1f89ff0173719e4
Serbay Yavuz <xxzmk@outlook.de>
2022-06-07 19:59:03 +0200

    Datenbank versuche mit SQLite

--- StudentsManager/src/datenbankpaket/Datenbankverbindung.java
+++ StudentsManager/src/datenbankpaket/Datenbankverbindung.java
@@ -2,10 +2,11 @@ package datenbankpaket;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Datenbankverbindung {
-    private static final String VERB = "jdbc:sqlite:studmanagersqlite";
+    private static final String VERB = "jdbc:sqlite:logindb";
 
     public static Connection getVerbindung() throws SQLException{
         try{
@@ -15,5 +16,24 @@ public class Datenbankverbindung {
             e.printStackTrace();
         }
         return null;
+    }
+    private static Connection connection;
+    
+    public synchronized static void runSQL(String query) throws SQLException {
+    	connection.createStatement().execute(query);
+    }
+    public static ResultSet runSQLquery(String query) throws SQLException {
+    	return connection.createStatement().executeQuery(query);
+    }
+    
+    static {
+    	try {
+    		connection = getVerbindung();
+    	}
+    	catch (RuntimeException e) {
+    		e.printStackTrace();
+    	} catch (SQLException e) {
+			e.printStackTrace();
+		}
     }
 }
