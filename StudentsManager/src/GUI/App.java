65d87a197053718fa072bf1c28c81ce297bc1d2e
Serbay Yavuz <xxzmk@outlook.de>
2022-06-09 21:21:24 +0200

    hier alles scheiße lol, ne spaß GUI läuft

--- StudentsManager/src/GUI/App.java
+++ StudentsManager/src/GUI/App.java
@@ -10,15 +10,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
 	public void start(Stage stage) throws Exception{
         try{
-            Datenbankverbindung.runSQL("CREATE TABLE Kurs ( id int primary key, kurs_name varchar(30));");
+            Datenbankverbindung.runSQL("CREATE TABLE IF NOT EXISTS Kurs ( id int primary key, kurs_name varchar(30));");
         } catch (SQLException e){
             e.printStackTrace();
         }
 
 
-        Parent root = (Parent) FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = (Parent) FXMLLoader.load(getClass().getClassLoader().getResource("gui.fxml"));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
--- StudentsManager/src/datenbankpaket/Datenbankverbindung.java
+++ StudentsManager/src/datenbankpaket/Datenbankverbindung.java
@@ -6,23 +6,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Datenbankverbindung {
+		
+	
     private static final String VERB = "jdbc:sqlite:login.db";
 
     public static Connection getVerbindung() throws SQLException{
         try{
             Class.forName("org.sqlite.JDBC");
             return DriverManager.getConnection(VERB);
+            
+        } catch (SQLException e) {
+        	e.printStackTrace();
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
         return null;
     }
     private static Connection connection;
     
-    public synchronized static void runSQL(String query) throws SQLException {
+    public synchronized static void runSQL(String query) throws SQLException {  // für alles andere
     	connection.createStatement().execute(query);
     }
-    public static ResultSet runSQLquery(String query) throws SQLException {
+    public static ResultSet runSQLquery(String query) throws SQLException { //zum selecten
     	return connection.createStatement().executeQuery(query);
     }
     
@@ -35,9 +40,9 @@ public class Datenbankverbindung {
     	} catch (SQLException e) {
 			e.printStackTrace();
 		}
-    }
     
-
+    
+	}
 }
 
 
