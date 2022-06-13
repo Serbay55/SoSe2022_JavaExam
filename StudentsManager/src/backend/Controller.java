09445c442ba42b9a67616eda9ac7b56aa4179b78
Serbay Yavuz <xxzmk@outlook.de>
2022-06-13 13:53:25 +0200

    Controller connected to Scene Builder fxml files

--- StudentsManager/src/backend/Controller.java
+++ StudentsManager/src/backend/Controller.java
@@ -5,9 +5,21 @@ import javafx.event.ActionEvent;
 public class Controller {
 	
 	public void listCourse(ActionEvent e) {
-		//System.out.println("Hello World");
+		System.out.println("Hello World");
+		
 	}
 	
+	public void delCourse(ActionEvent e) {
+		System.out.println("deleted");
+	}
+	
+	public void addStudent(ActionEvent e) {
+		System.out.println("student added");
+	}
+	
+	public void exmStudent(ActionEvent e) {
+		System.out.println("exmatriculated");
+	}
 	
 
 }
